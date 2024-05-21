46296aa1e75f0e153982112ee4267f36671a6fae
Gustavo <gustavohdlima@hotmail.com>
2024-05-21 00:09:38 -0300

    feat: Implementando like/dislike e delete de tweets
    E separando scripts

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -17,7 +17,17 @@ window.addEventListener("DOMContentLoaded", async () => {
       },
     });
 
+    const tweetsresponse = await fetch("http://localhost:3001/tweet", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
     const userdata = await userresponse.json();
+    const tweetsdata = await tweetsresponse.json();
+
     localStorage.setItem("data", JSON.stringify(userdata.content));
     document.getElementById("dropdown-data-user-button").innerText = userdata.content.firstName;
 
@@ -31,9 +41,8 @@ window.addEventListener("DOMContentLoaded", async () => {
       document.getElementById("allTweetsTab").classList.add("active");
       document.getElementById("myTweetsTab").classList.remove("active");
     });
-
-    // Adiciona o card completo ao contêiner de postagens.
-    userdata.content.tweets.forEach((tweet) => {
+    console.log(tweetsdata.content);
+    tweetsdata.content.forEach((tweet) => {
       var postDiv = document.createElement("div");
       postDiv.className = "card text-bg-dark mb-3";
       postDiv.style = "width: 100%; margin-bottom: 0 !important;";
@@ -70,51 +79,32 @@ window.addEventListener("DOMContentLoaded", async () => {
 
       cardHeader.innerText = userdata.content.firstName + " " + userdata.content.lastName;
       cardText.innerText = tweet.content;
+
+      if (tweet.likes.some((author) => author.id === userdata.content.id))
+        likeButton.classList.add("active");
+
+      likeButton.addEventListener("click", async () => {
+        fetch(`http://localhost:3001/tweet/${tweet.id}`, {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then(() => likeButton.classList.toggle("active"));
+      });
+
+      deleteButton.addEventListener("click", () => {
+        fetch(`http://localhost:3001/tweet/${tweet.id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then(() => {
+          tweetsdata.content.splice(tweetsdata.content.findIndex((t) => t.id === tweet.id));
+          document.getElementById("postContainer").removeChild(postDiv);
+        });
+      });
+
       document.getElementById("postContainer").appendChild(postDiv);
     });
   }
 });
-
-if (document.getElementById("form-login"))
-  document.getElementById("form-login").addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const email = document.getElementById("login-email-field").value;
-    const password = document.getElementById("login-password-field").value;
-
-    if (!email || !password) {
-      alert("Preencha todos os campos");
-      return;
-    }
-
-    const loginresponse = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
-    const data = await loginresponse.json();
-    if (!data.success) alert(data.content);
-    else {
-      localStorage.setItem("token", data.content.token);
-      const userresponse = await fetch("http://localhost:3001/user/me", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${data.content.token}`,
-        },
-      });
-      const userdata = await userresponse.json();
-      localStorage.setItem("data", JSON.stringify(userdata.content));
-      window.location.href = "/index.html";
-    }
-  });
-
-function logout() {
-  console.log("logout");
-}
-
-function loadMyProfileData() {
-  console.log("loadMyProfileData");
-}
---
+++ frontend/js/login.js
@@ -0,0 +1,43 @@
+if (document.getElementById("form-login"))
+  document.getElementById("form-login").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("login-email-field").value;
+    const password = document.getElementById("login-password-field").value;
+
+    if (!email || !password) {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    const loginresponse = await fetch("http://localhost:3001/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await loginresponse.json();
+    if (!data.success) alert(data.content);
+    else {
+      localStorage.setItem("token", data.content.token);
+      const userresponse = await fetch("http://localhost:3001/user/me", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${data.content.token}`,
+        },
+      });
+      const userdata = await userresponse.json();
+      localStorage.setItem("data", JSON.stringify(userdata.content));
+      window.location.href = "/index.html";
+    }
+  });
+
+function logout() {
+  console.log("logout");
+}
+
+function loadMyProfileData() {
+  console.log("loadMyProfileData");
+}
