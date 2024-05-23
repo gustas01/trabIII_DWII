2767f1deb4562deeda72c5f7a8d1aca366af7a7f
Gustavo <gustavohdlima@hotmail.com>
2024-05-23 00:00:51 -0300

    feat, refactor: alterando mais nomes de arquivos e implementando funcionalidade para criar conta

---
+++ frontend/js/signup.js
@@ -0,0 +1,42 @@
+window.addEventListener("DOMContentLoaded", async () => {
+  if (window.location.pathname === "/index.html" && localStorage.getItem("token")) {
+    window.location.href = "/home.html";
+  }
+  if (
+    window.location.pathname !== "/index.html" &&
+    window.location.pathname !== "/signup.html" &&
+    !localStorage.getItem("token")
+  ) {
+    window.location.href = "/index.html";
+  }
+
+  if (document.getElementById("form-login"))
+    document.getElementById("form-login").addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const email = document.getElementById("login-email-field").value;
+      const password = document.getElementById("login-password-field").value;
+      const firstName = document.getElementById("login-firstname-field").value;
+      const lastName = document.getElementById("login-lastname-field").value;
+
+      if (!email || !password || !firstName || !lastName) {
+        alert("Preencha todos os campos");
+        return;
+      }
+
+      const loginresponse = await fetch("http://localhost:3001/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, firstName, lastName }),
+      });
+      const data = await loginresponse.json();
+      console.log(data.success);
+      if (!data.success) alert(data.content);
+      else {
+        alert(data.content);
+        window.location.href = "/home.html";
+      }
+    });
+});
